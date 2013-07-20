c8d70113869a290b6aafd335eb440604d81cbe99
Justin Smith <noisesmith@gmail.com>
2013-07-19 18:10:43 -0700

    hotfix patch extension for this release for a needed bug fix

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject antler/lichen "0.3.5"
+(defproject antler/lichen "0.3.5-QUALITY"
   :description "A service for caching and retrieving images"
   :url "http://github.com/antler/lichen"
   :license {:name "Eclipse Public License"
