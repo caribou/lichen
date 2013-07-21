9fbf992452249cc1012171e8761f8c60a8b43aea
Justin Smith <noisesmith@gmail.com>
2013-07-20 20:58:01 -0700

    version bump for release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject antler/lichen "0.3.5-QUALITY"
+(defproject antler/lichen "0.4.1"
   :description "A service for caching and retrieving images"
   :url "http://github.com/antler/lichen"
   :license {:name "Eclipse Public License"
