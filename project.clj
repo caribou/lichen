c6c726c37dc1fab1074935fd41f56564533efd59
Justin Smith <noisesmith@gmail.com>
2013-09-12 15:41:50 -0700

    versions

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject caribou/lichen "0.6.3"
+(defproject caribou/lichen "0.6.5"
   :description "A service for caching and retrieving images"
   :url "http://github.com/caribou/lichen"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.mortennobel/java-image-scaling "0.8.5"]
                  ;; [org.imgscalr/imgscalr-lib "4.2"]
                  [clj-aws-s3 "0.3.6"]]
